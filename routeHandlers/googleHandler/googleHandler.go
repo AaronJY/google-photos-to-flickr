abdad2fc493e7deef6074408d87080267ca367f5
Aaron Yarborough <personal.ajy@gmail.com>
2019-09-27 08:57:41 +0100

    Refactor googleHandler.go to bring in-line with Go style guide and naming conventions

--- routeHandlers/googleHandler/googleHandler.go
+++ routeHandlers/googleHandler/googleHandler.go
@@ -3,16 +3,17 @@ package googleHandler
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 const routePrefix = "/api/google"
 
 // TODO: These need to be moved to environment variables
-const googleClientId = "670357560287-i14cmjb033n19ssa2m0nng419uknqk59.apps.googleusercontent.com"
+const googleClientID = "670357560287-i14cmjb033n19ssa2m0nng419uknqk59.apps.googleusercontent.com"
 const photoReadScope = "https://www.googleapis.com/auth/drive.photos.readonly"
-const redirectUri = "http://localhost:1337/api/google/callback"
+const redirectURI = "http://localhost:1337/api/google/callback"
 
 // RegisterRoutes sets up routing for the /google route prefix
 func RegisterRoutes(router *mux.Router) {
@@ -21,7 +22,7 @@ func RegisterRoutes(router *mux.Router) {
 	subRouter.HandleFunc("/auth", authenticate).Methods("GET")
 	subRouter.HandleFunc("/callback", authCallback).Methods("GET")
 
-	fmt.Println("Successfully registered google routes")
+	fmt.Println("Successfully registered google routes.")
 }
 
 // health writes with a generic response message to let
@@ -36,7 +37,7 @@ func health(respWriter http.ResponseWriter, req *http.Request) {
 // authenticate redirects the other to the Google OAuth login
 // screen
 func authenticate(respWriter http.ResponseWriter, req *http.Request) {
-	oAuthUrl := buildOAuthUrl(googleClientId, photoReadScope, redirectUri)
+	oAuthUrl := buildOAuthURL(googleClientID, photoReadScope, redirectURI)
 	respWriter.Header().Set("Location", oAuthUrl)
 	respWriter.WriteHeader(302)
 }
@@ -48,30 +49,30 @@ func authCallback(respWriter http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 
 	code := vars["code"]
-	if (code == "") {
+	if code == "" {
 		http.Error(respWriter, "code not found.", 400)
 		return
 	}
 
 	err := json.NewEncoder(respWriter).Encode(req)
-	if (err != nil) {
+	if err != nil {
 		fmt.Println(err)
 		http.Error(respWriter, "could not encode response.", 500)
 		return
 	}
 }
 
-// buildOAuthUrl builds and returns the Google OAuth screen URL
-func buildOAuthUrl(clientId string, scope string, redirectUri string) string {
+// buildOAuthURL builds and returns the Google OAuth screen URL
+func buildOAuthURL(clientId string, scope string, redirectUri string) string {
 	return "https://accounts.google.com/o/oauth2/v2/auth?client_id=" +
-		googleClientId + "&response_type=code&scope=" + scope + "&redirect_uri=" + redirectUri
+		googleClientID + "&response_type=code&scope=" + scope + "&redirect_uri=" + redirectUri
 }
 
-// buildRequestTokenUrl builds and returns the Google request token URL
-func buildRequestTokenUrl(code string, clientId string, clientSecret string, redirectUri string, grantType string) string {
+// buildRequestTokenURL builds and returns the Google request token URL
+func buildRequestTokenURL(code string, clientId string, clientSecret string, redirectUri string, grantType string) string {
 	return "https://oauth2.googleapis.com/token?" +
 		"code=" + code + "&" +
-		"client_id=" +  clientId + "&" +
+		"client_id=" + clientId + "&" +
 		"client_secret" + clientSecret + "&" +
 		"redirect_uri" + redirectUri + "&" +
 		"grant_type" + grantType
--- routeHandlers/routeHandler.go
+++
@@ -1,7 +0,0 @@
-package routeHandlers
-
-import "github.com/gorilla/mux"
-
-type RouteHandler interface {
-	RegisterRoutes(router *mux.Router)
-}
